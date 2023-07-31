b4e5b33b1ee9653c28bb0c823050a92649eeede0
Dewuruge <dewurugekalpadeep@gmail.com>
2023-07-31 23:16:42 +0530

    Complete Task 4

---
+++ Demo4.java
@@ -0,0 +1,11 @@
+public class Demo4{
+    public static void main(String[] args) {
+        int day = 28;
+        int month = 12;
+        int year = 2023;
+        int hour = 23;
+        int minute = 59;
+        int second = 59;
+        System.out.printf("\033[34m%d\033[0m/\033[34m%d\033[0m/\033[34m%d\033[0m \033[32m%d\033[0m:\033[32m%d\033[0m:\033[32m%d\033[0m\n",day,month,year,hour,minute,second);
+    }
+}
