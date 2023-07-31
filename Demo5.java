20e1077f78fcc21909a6a0ff769669bd914aa61d
Dewuruge <dewurugekalpadeep@gmail.com>
2023-07-31 23:30:33 +0530

    Complete Task 5

---
+++ Demo5.java
@@ -0,0 +1,11 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        int num1 = 978;
+        int num2 = 3;
+        int num3 = 16;
+        int num4 = 1484100;
+
+        System.out.printf("\033[35m%d\033[0m-\033[36m%d\033[0m-\033[32m%d\033[0m-\033[31m%d\033[0m\n",num1,num2,num3,num4);
+    }
+    
+}
