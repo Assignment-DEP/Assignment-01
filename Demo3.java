54eedd73be1e58a7ef9ddcf2465673335542b49f
Dewuruge <dewurugekalpadeep@gmail.com>
2023-07-31 20:26:51 +0530

    Complete Task 3

---
+++ Demo3.java
@@ -0,0 +1,11 @@
+public class Demo3 {
+    public static void main(String[] args) {
+
+        int phoneNumber1 = 123;
+        int phoneNumber2 = 456;
+        int phoneNumber3 = 7890;
+
+        System.out.printf("(\033[34m%d\033[0m)%d-%d\n",phoneNumber1,phoneNumber2,phoneNumber3);
+    }
+    
+}
